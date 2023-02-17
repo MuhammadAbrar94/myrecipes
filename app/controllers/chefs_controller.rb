8da2c63334f558b5b575d0a98e3cb2eef86e8076
Abrar <m.abrarali94@gmail.com>
2023-02-17 15:33:39 +0500

    Implemented Sessions and login

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,18 +1,48 @@
 class ChefsController < ApplicationController
+
+    def index 
+      @chef = Chef.all
+    end
+
     def new 
-        @chef = Chef.new
+      @chef = Chef.new
     end
 
     def create
-      
-        @chef = Chef.new(chef_params)
-        if @chef.save
-          flash[:success] = "Welcome #{@chef.chefname} to MyRecipes App!"
-          redirect_to chef_path(@chef)
-        else
-          render 'new'
-        end
+      @chef = Chef.new(chef_params)
+      if @chef.save
+        flash[:success] = "Welcome #{@chef.chefname} to MyRecipes App!"
+        redirect_to chef_path(@chef)
+      else
+        render 'new'
+      end
     end
+
+    def show
+      @chef = Chef.find(params[:id])
+    end
+
+    def edit
+      @chef = Chef.find(params[:id])
+    end
+    
+    def update
+      @chef = Chef.find(params[:id])
+      if @chef.update(chef_params)
+        flash[:success] = "Your account was updated successfully"
+        redirect_to @chef
+      else
+        render 'edit'
+      end  
+    end
+
+    def destroy
+      @chef = Chef.find(params[:id])
+      @chef.destroy
+      flash[:danger] = "Chef and all associated recipes have been deleted"
+      redirect_to chefs_path
+    end
+
 
     private
   
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
   resources :recipes
   get '/signup' , to:'chefs#new'
   resources :chefs
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
 end
