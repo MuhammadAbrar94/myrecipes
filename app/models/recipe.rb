d372ad3e4103eb4677169e37c47e9335b36b5567
Abrar <m.abrarali94@gmail.com>
2023-02-17 18:14:48 +0500

    RecipeIngredients & Comments model/association created

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,8 @@ class Recipe < ApplicationRecord
     validates :desciption, presence: true, length: {minimun: 5, maximum:500}
     belongs_to :chef
     validates :chef_id, presence: true
+    has_many :recipe_ingredients
+    has_many :ingredients, through: :recipe_ingredients
+
+    has_many :comments, dependent: :destroy
 end
